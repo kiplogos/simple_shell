5ee2175b32b9f98cc81b1a4aca839d5340606d65
Kit3l <pchumbakitel@gmail.com>
2022-09-07 12:38:17 -0700

    betty would be proud

---
+++ shell.h
@@ -0,0 +1,13 @@
+#ifndef _SHELL_H_
+#define _SHELL_H_
+
+#include <signal.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+
