ac2dd872dbb1c090deee6e42bde0241a937087b5
Kit3l <pchumbakitel@gmail.com>
2023-03-30 23:29:23 -0700

    hot fix

--- unixintpreter.c
+++ unixintpreter.c
@@ -1,47 +1,42 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include <stdlib.h>
 #include <sys/wait.h>
 
-#define MAX_LEN 1024
+#define MAX_COMMAND_LENGTH 100
 
-void simple_shell()
-{
-	char input[MAX_LEN];
+int main() {
+    char user_input[MAX_COMMAND_LENGTH];
 
-	 while (1) {
-	     printf("$ ");
-	     if (fgets(input, MAX_LEN, stdin) == NULL) {
-	         printf("\n");
-		 break;
-	     }
+    while(1) {
+        printf("$ ");
+        fgets(user_input, MAX_COMMAND_LENGTH, stdin); // read input from user
+        user_input[strcspn(user_input, "\n")] = '\0'; // remove trailing newline character
 
-	     // Remove the newline character from the input
-	     // input[strcspn(input, "\n")] = 0;
-	     
-	     if (strlen(input) == 0) {
-		 continue;
-	     }
+        if (strlen(user_input) == 0) {
+            continue; // if input is empty, prompt again
+        }
 
-	     pid_t pid = fork();
-	     if (pid == -1) {
-		 perror("fork");
-		 exit(1);
-	     } else if (pid == 0) {
-    	       if (execlp(input, input, (char *) NULL) == -1) {
-		   perror("execlp");
-		   exit(1);
-	       }
-	     } else {
-		     int status;
-		     waitpid(pid, &status, 0);
-	     }
-	 }
+        pid_t pid = fork(); // create a new process
+
+        if (pid < 0) {
+            printf("Error: fork failed\n");
+            exit(1);
+        }
+
+        if (pid == 0) { // child process
+            execlp(user_input, user_input, (char *) NULL); // execute command
+            printf("Error: command not found\n"); // if exec returns, command failed
+            exit(1);
+        }
+
+        else { // parent process
+            int status;
+            waitpid(pid, &status, 0); // wait for child process to finish
+        }
+    }
+
+    return 0;
 }
 
-int main(void)
-{
-	simple_shell();
-	return 0;
-}
